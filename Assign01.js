ed870bdb4a39a8c24f33570c21197187db1f51a5
Muhammad Numair Qureshi <90131645+mnumairqureshi@users.noreply.github.com>
2023-04-07 21:23:43 +0500

    Add files via upload

---
+++ Assign01.js
@@ -0,0 +1,101 @@
+// // ACTIVITY 01
+// let firstValue = prompt("Enter Your First Value");
+// let secondValue = prompt("Enter Your Second Value");
+// if (firstValue > secondValue) {
+//     console.log(firstValue)
+// } else {
+//     console.log(secondValue)
+// }
+
+// // ACTIVITY 02
+// let firstValue = prompt("Enter Any Positive or Negative Number");
+// if (firstValue >= 0) {
+//     document.write("The Sign is +")
+// }
+//  else {
+//     document.write("The Sign is -");
+// }
+
+// // ACTIVITY 03
+// let num1 = prompt("num1");
+// let num2 = prompt("num2");
+// let num3 = prompt("num3");
+// let num4 = prompt("num4");
+// let num5 = prompt("num5");
+// if (num1 > num2) {
+//     console.log(num1)
+// } else {
+//     if (num2 > num3) {
+//         console.log(num2)
+//     } else {
+//         if (num3 > num4) {
+//             console.log(num3)
+//         } else {
+//             if (num4 > num5) {
+//                 console.log(num4)
+//             } else if (num5 > num4) {
+//                 console.log(num5)
+//             }
+//         }
+//     }
+// }
+
+// // ACTIVITY 04
+
+// for (let i=0; i<=15; i=i+1) {
+//     if (i % 2 ) {
+//         document.write( "<br/>" + i + " is ODD " + "<br/>" )
+//     } else {
+//         document.write( "<br/>" + i + " is EVEN " + "<br/>" )
+//     }
+// }
+
+// // ACTIVITY 05
+// let tN = prompt("Enter Students Total Marks ");
+// if (tN <= 60) {
+//     document.write("Fail");
+// } else
+//     if (tN <= 70) {
+//         document.write("Grade D");
+//     } else
+//         if (tN <= 80) {
+//             document.write("Grade C");
+//         } else
+//             if (tN <= 90) {
+//                 document.write("Grade B");
+//             } else
+//                 if (tN <= 100) {
+//                     document.write("Grade A")
+//                 }
+
+// // ACTIVITY 06
+
+// for (let i = 1; i < 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         document.write("FizzBuzz" + "<br/>");
+//     } else if (i % 3 === 0) {
+//         document.write("Fizz" + "<br/>");
+//     } else if (i % 5 === 0) {
+//         document.write("Buzz" + "<br/>");
+//     } else {
+//         document.write(i + "<br/>");
+//     }
+// }
+
+// // ACTIVITY 07
+
+// for (let i = 1; i <= 5; i++) {
+//     document.write(i + "<br/>");
+//     for (let j = 1; j <= i; j++) {
+//         document.write(j);
+//     }
+// }
+
+
+
+
+
+
+
+
+
